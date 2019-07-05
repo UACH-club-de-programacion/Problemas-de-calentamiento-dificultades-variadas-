811f459a7f713297c3825e7e30eb1534c58aeb5b
GabrielMarBarrio <49036708+GabrielMarBarrio@users.noreply.github.com>
2019-07-05 17:12:25 -0600

    Update 2016 D.cpp

--- 2016 D.cpp
+++ 2016 D.cpp
@@ -7,15 +7,18 @@
 Regionals 2016 Latin America. Problem D: Dating On-Line
 https://icpcarchive.ecs.baylor.edu/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=5911
 
-Breve explicacion: El problema nos brinda una serie de actividades con una calificacion, la cual puede ser como un diagrama radial de muchas maneras.
-Nosotros debemos de encontrar el area mas grande de todas las areas posibles.
-El problema se puede simplificar en dividir el diagrama radial en muchos triangulos pequeños, de los cuales conocemos 2 lados y un angulo (360/N), suficiente
-informacion para conocer su area (mediante 1/2abSinC). Para saber cual es la manera en acomodarlos que nos dara mas area, tenemos que intentar hacer los triangulos mas grandes posibles,
-en otras palabras, hacer triangulos con los lados mas grandes posibles (no queremos hacer un triangulo con el lado mas grande y el mas pequeño).
-Para lograr esto podemos acomodar el arreglo de tal manera que los mas pequeños esten en los extremos y los mas grandes en el medio (el mas pequeño en el primer lugar,
-el segundo mas pequeño en el ultimo, el tercero en el segundo, el cuarto en el penultimo... el mas grande en el centro.)
-Al tener el arreglo ordenado de esta manera solo tenemos aplicar N veces la formula : 1/2abSinC 'a' siendo la ith casilla, 'b' la casilla i+1th y 'C' siendo 360/N. Asi solo tenemos que
-ir sumando el area de todos los triangulos y podremos conocer la posible area mayor.
+Breve explicacion: El problema nos brinda una serie de actividades con una calificacion, la cual puede ser como un diagrama
+radial de muchas maneras. Nosotros debemos de encontrar el area mas grande de todas las areas posibles.
+El problema se puede simplificar en dividir el diagrama radial en muchos triangulos pequeños, de los cuales conocemos 2 
+lados y un angulo (360/N), suficiente informacion para conocer su area (mediante 1/2abSinC). 
+Para saber cual es la manera en acomodarlos que nos dara mas area, tenemos que intentar hacer los triangulos mas grandes 
+posibles, en otras palabras, hacer triangulos con los lados mas grandes posibles (no queremos hacer un triangulo con el
+lado mas grande y el mas pequeño). Para lograr esto podemos acomodar el arreglo de tal manera que los mas pequeños esten 
+en los extremos y los mas grandes en el medio (el mas pequeño en el primer lugar, el segundo mas pequeño en el ultimo, el 
+tercero en el segundo, el cuarto en el penultimo... el mas grande en el centro).
+Al tener el arreglo ordenado de esta manera solo tenemos aplicar N veces la formula : 1/2abSinC 'a' siendo la ith casilla, 
+'b' la casilla i+1th y 'C' siendo 360/N. Asi solo tenemos que ir sumando el area de todos los triangulos y podremos conocer 
+la posible area mayor.
 
 **/
 
